0508da92605cd4048a9593a0317af68c8bac417d
Соколов Александр <Zogymac@gmail.com>
2019-10-13 15:09:00 +0300

    Solved 1-3 task's

--- main.go
+++ main.go
@@ -9,21 +9,63 @@ import (
 
 var (
 	seed = flag.Int("seed", 0, "seed for random generator. unix(now) be default")
+	start = flag.Int("start", 1, "start of range")
+	end = flag.Int("end", 6, "end of range")
+	n = flag.Int("n", 1, "number of throws")
+	norepeat = flag.Bool("norepeat", false, "does it repeat")
 )
 
-// Фукнция должна вернуть число из интервала [l,r]
-//func randInterval
-//	return 0
-//}
-
-
-func main() {
+//Фукнция должна вернуть число из интервала [l,r]
+func randInterval(start, end int) int{
 	flag.Parse()
 	if *seed == 0 {
-		rand.Seed(time.Now().Unix())
+		rand.Seed(time.Now().UnixNano())
 	} else {
 		rand.Seed(int64(*seed))
 	}
-	// Dice roll 1..6
-	fmt.Println(rand.Intn(6) + 1)
+	return rand.Intn(end) + start
+}
+
+
+func main() {
+	var sum, a, b int
+	flag.Parse()
+	if *start > *end {
+		fmt.Println("Ошибка, start > end")
+	} else {
+		if *norepeat {
+			if *end - *start < *n {
+				fmt.Println("Невозможно вывести N чисел от L до R без повторений.")
+			} else {
+				a = randInterval(*start, *end)
+				sum += a
+				fmt.Print(a, " ")
+				b = a
+				for i := 1; i < *n; i++ {
+					a = randInterval(*start, *end)
+					sum += a
+					sum %= a
+					if b != 0 && b != a {
+						fmt.Print(a, " ")
+						sum += b
+					} else {
+						i--
+					}
+					/*
+						if sum % a != 0 && sum != b{
+							fmt.Println(a, b, sum, sum % a, sum % b)
+						} else {
+							i--
+							sum -= a
+							fmt.Println(i, a, b, sum)
+						}*/
+					b = a
+				}
+			}
+		} else {
+			for i := 0; i < *n; i++ {
+				print(randInterval(*start, *end), " ")
+			}
+		}
+	}
 }
